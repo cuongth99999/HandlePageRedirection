7111a6b1125c7121081c4c24f540f6828628e73b
cuongth <cuongth@magenest.com>
2023-07-10 10:41:32 +0700

    update Exercise 2: Controller & URL Rewrite

--- Block/Sale/SaleBox.php
+++ Block/Sale/SaleBox.php
@@ -11,12 +11,12 @@ class SaleBox extends Template
         Template\Context $context,
         \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory  $productCollectionFactory,
         \Magento\Catalog\Block\Product\ListProduct $listProduct,
-        \Magento\Catalog\Model\ProductFactory $productFactory,
+        \Magento\Catalog\Model\ProductRepository $productRepository,
         array $data = []
     )
     {
         $this->productCollectionFactory = $productCollectionFactory;
-        $this->productFactory = $productFactory;
+        $this->productRepository = $productRepository;
         $this->listProduct = $listProduct;
         parent::__construct($context, $data);
     }
@@ -24,16 +24,17 @@ class SaleBox extends Template
     public function getProductSaleOffJsonData()
     {
         $data = [];
-        $products = $this->productFactory->create();
-        foreach ($products as $product) {
-            if ($product->getSpecialPrice() < $product->getPrice() && !empty($product->getSpecialPrice())) {
-                $productCollection = $this->productCollectionFactory->create();
-                foreach ($productCollection as $item)
-                {
-                    $productImage = $this->listProduct->getImage($item, 'product_thumbnail_image');
+        $productCollection = $this->productCollectionFactory->create();
+        foreach ($productCollection as $item)
+        {
+            $product = $this->productRepository->getById($item['entity_id']);
+            $specialPrice = $product->getPriceInfo()->getPrice('special_price');
+            if (!empty($specialPrice->getSpecialPrice())) {
+                if ($product->getPrice() > $specialPrice->getSpecialPrice()) {
+                    $productImage = 'media/catalog/product'.$product->getThumbnail();
                     $data[] = [
                         'url' => $item->getProductUrl(),
-                        'image' => $productImage->getImageUrl(),
+                        'image' => $productImage,
                         'name' => $item->getSku()
                     ];
                 }
